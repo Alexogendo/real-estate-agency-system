8af918422090e4a577ec41076574c23dd5254148
Alexogendo <alexnyabuto8@gmail.com>
2019-11-17 12:47:33 +0300

    still updating

--- resources/views/frontend/partials/slider.blade.php
+++ resources/views/frontend/partials/slider.blade.php
@@ -1,17 +1,10 @@
 <section class="carousel carousel-slider center">
-    @if($sliders)
-        @foreach($sliders as $slider)
-            <div class="carousel-item" style="background-image: url({{Storage::url('slider/'.$slider->image)}})" href="#{{$slider->id}}!">
+   
+            <div class="carousel-item" style="background-image: url({{asset('frontend/images/real_estate.jpg')}})" href="#1!">
                 <div class="slider-content">
-                    <h2 class="white-text">{{ $slider->title }}</h2>
-                    <p class="white-text">{{ $slider->description }}</p>
+                     <h2>Bernadette Real Estate</h2>
+            <p class="indigo-text">Exceptional services for agents and users.</p>
                 </div>
             </div>
-        @endforeach
-    @else 
-        <div class="carousel-item amber indigo-text" style="background-image: url({{ asset('frontend/images/real_estate.jpg') }})" href="#1!">
-            <h2>Bernadette Real Estate</h2>
-            <p class="indigo-text">Exceptional services for agents and users.</p>
-        </div>
-    @endif
+      
 </section>
